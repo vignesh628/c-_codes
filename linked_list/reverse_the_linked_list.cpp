6938fd12dec05ae4950551a996874242e1d52c0e
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-07 12:44:33 +0530

    Create reverse_the_linked_list.cpp

---
+++ linked_list/reverse_the_linked_list.cpp
@@ -0,0 +1,8 @@
+#This probelm is tricky but easy -> we will be using the previous,current,next nodes where previous will be null, current will be
+#node we take now and next will be thenext node.
+#logic:
+#present->address=previous;
+#previous=present;
+#present=next;
+#next=next->address;
+#########################################################################################################################################
