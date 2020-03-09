4fdbab0ca18b78a3644912cab799d6785ebab034
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-09 12:20:46 +0530

    Update This_pointer.cpp

--- oops/This_pointer.cpp
+++ oops/This_pointer.cpp
@@ -3,4 +3,84 @@
 * this pointer is constant pointer holds the address of current object.
 * This pointer is not avaliable for the static fucntions since they can be called without the object initialisation.
 ##########################################################################################################################
-* 
+#include<iostream>
+using namespace std;
+class a 
+{
+  int x;
+  public:
+  void setx(int y)
+  {
+      x=y;
+  }
+  void disp()
+  {
+      cout<<"the value of x is "<<x;
+  }
+};
+int main()
+{
+    int y=10;
+    a obj;
+    obj.setx(y);
+    obj.disp();
+    
+}
+############################################################################################################################
+output:
+the value of x is 10
+###########################################################################################################################
+But if the global variable is same as the variable declared like this
+  #include<iostream>
+using namespace std;
+class a 
+{
+  int x;
+  public:
+  void setx(int x)
+  {
+      x=x;
+  }
+  void disp()
+  {
+      cout<<"the value of x is "<<x;
+  }
+};
+int main()
+{
+    int y=10;
+    a obj;
+    obj.setx(y);
+    obj.disp();
+    
+}
+output:
+the value of x is 1227675968
+###########################################################################################################################
+Here we can use the this pointer so that this pointer is the pointer poining to  the address of the object.
+ #include<iostream>
+using namespace std;
+class a 
+{
+  int x;
+  public:
+  void setx(int x)
+  {
+     this->x=x;
+  }
+  void disp()
+  {
+      cout<<"the value of x is "<<x;
+  }
+};
+int main()
+{
+    int y=10;
+    a obj;
+    obj.setx(y);
+    obj.disp();
+    
+}
+############################################################################################################################
+output:
+the value of x is 10
