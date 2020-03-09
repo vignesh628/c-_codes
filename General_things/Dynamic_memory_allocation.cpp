1a6f19901386483a657323ea43531b2dab0a4d6c
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-09 15:49:31 +0530

    Create Dynamic_memory_allocation.cpp

---
+++ General_things/Dynamic_memory_allocation.cpp
@@ -0,0 +1,9 @@
+* Memory in c++ program,
+* The stack-> All the variables declared inside the function will take up the memory from the stack.
+* The Heap->This is unused part of memory can be allocated when the program runs dynamically,.
+#####################################################################################################################
+You can use the new operator to allocate the memory at run time for the variable of given type.
+You can use the delete operator when you no more need the memory that you have dynamically allocated.
+#####################################################################################################################
+* When a data member is declared as static, only one copy of the data is maintained for all objects of the class.
+#####################################################################################################################
