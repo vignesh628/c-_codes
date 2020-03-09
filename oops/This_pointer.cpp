d9823952c35cf62fa21f5af74dc0f02001e5b6a6
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-09 11:34:19 +0530

    Create This_pointer.cpp

---
+++ oops/This_pointer.cpp
@@ -0,0 +1,6 @@
+* While calling the functions 'this' will be passsed hidden argument.
+* The passed this pointer will be available as the local variable in the called function.
+* this pointer is constant pointer holds the address of current object.
+* This pointer is not avaliable for the static fucntions since they can be called without the object initialisation.
+##########################################################################################################################
+* 
