7827755e94588ed9b45da20d1d6e181868c88202
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-09 13:23:28 +0530

    Create overloading_and_overriding.cpp

---
+++ oops/overloading_and_overriding.cpp
@@ -0,0 +1,6 @@
+Overloading:
+Relationship between the methods of same class.
+Does not require more than one class for overloading.
+Overriding:
+Relationship between the methods of super clas and sub class(In other words base class and derived class).
+Requires more than one class for overriding.
