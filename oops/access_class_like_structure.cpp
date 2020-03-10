46ba770bcd3734e1634c482ade9baaba04081fb8
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-11 02:53:16 +0530

    Create access_class_like_structure.cpp

---
+++ oops/access_class_like_structure.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class a  
+{
+    public:
+    void disp()
+    {
+        cout<<"Hello all this is class a display finction"<<endl;
+    }
+};
+int main()
+{
+    a *obj;
+    obj->disp();
+    
+}
+######################################################################################################################################
+Hello all this is class a display finction
+
+#######################################################################################################################################
+https://ide.geeksforgeeks.org/reBGAK58n3
