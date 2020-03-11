83fd6445086c136fcb3c18b5141b8a4a9e6801dd
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-12 01:50:36 +0530

    Update Dynamic_memory_allocation.cpp

--- General_things/Dynamic_memory_allocation.cpp
+++ General_things/Dynamic_memory_allocation.cpp
@@ -7,3 +7,26 @@ You can use the delete operator when you no more need the memory that you have d
 #####################################################################################################################
 * When a data member is declared as static, only one copy of the data is maintained for all objects of the class.
 #####################################################################################################################
+How we allocate the memory at the run time using the new operator??
+  basically new is used to allocate the memory to the pointer variables.
+  int *a=new int[20];
+this value of the 20 can be decided at the run time.
+  for example,
+class a:
+private:
+int x;
+list<int> *adjlist;
+public:
+void set(int y)
+{
+  x=y;
+  list<int> *adjlist=new list<int>[y];
+}
+};
+int main()
+{
+  a obj;
+  obj.set(20);
+}
+like this based on the value that we pass we can initialise the number of lists as per the requiorement.
+########################################################################################################################
