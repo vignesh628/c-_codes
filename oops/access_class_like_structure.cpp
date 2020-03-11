987b10d3285325fe853064fc54dd95be795dbf5f
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-11 23:36:04 +0530

    Update access_class_like_structure.cpp

--- oops/access_class_like_structure.cpp
+++ oops/access_class_like_structure.cpp
@@ -1,3 +1,6 @@
+here we are able to acess the class like structure because we have declared the pointer. pointer is basically pints to the null when it is initializ
+ed. we can use the -> operator to access the values that are stored inside the class or structure .
+ ####################################################################################################################################
 #include<iostream>
 using namespace std;
 class a  
