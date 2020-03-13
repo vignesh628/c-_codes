e674065c66be08b79ad7ffbe4e7602a079b9b52d
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-13 22:21:48 +0530

    Create template_function.cpp

---
+++ General_things/template_function.cpp
@@ -0,0 +1,15 @@
+*Template is simple and powerful tool in c++.
+Why we use the templates:
+No need to write the same code for the different data types.
+########################################################################################################################################
+for example, a software company need to sort the differenet data types. rather than writing and maintaining the multiple codes 
+we can write one sort function and pass the parameter.
+#####################################################################################################################################
+c++ adds the two new keywords to support the templates:
+1.template
+2.typename
+#####################################################################################################################################
+* templates are expanded during the compiler time.
+* this is like macros.
+* source code contains only  function/class but the compiled code contain the multiple copies of same function/class.
+#######################################################################################################################################
