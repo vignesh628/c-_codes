291611a71a8c0a0632e541f24bb0d40472e637d7
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-16 12:06:47 +0530

    Create Introduction1.cpp

---
+++ General_things/Introduction1.cpp
@@ -0,0 +1,4 @@
+* We can declare the constants with using the keyword const. This is similar to that of declaring a variable, 
+but with a const prefix.
+* const int a=20;
+############################################################################################################################
