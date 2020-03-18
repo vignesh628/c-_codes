02756cac6b7ec4462ddca93a117074e66c3103ec
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-18 13:18:03 +0530

    Create oops_mcqs.cpp

---
+++ oops/oops_mcqs.cpp
@@ -0,0 +1,33 @@
+Create object
+Send Feedback
+Which of these statements is used to allocate memory for an object dynamically ?
+######################################################################################################
+output:
+New
+##########################################################################################################################################
+Create object
+Send Feedback
+Which of the following is/are a valid statement(s) to create an object of class Bird in C++ ?
+##############################################################################################
+Bird *b=new Bird;
+Bird b;
+########################################################################################
+What is the output
+Send Feedback
+What is the output -
+#include <iostream>
+using namespace std;
+
+class Student {
+    int rollNumber;
+};
+
+int main() {
+    Student s;
+    cout << s.rollNumber;
+}
+############################################################################################################################
+output:
+compiler error
+since the variable is private
+#######################################################################################################################################
