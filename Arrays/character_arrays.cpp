291185969c0829d89435eaba70652dd31ecba484
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-18 23:01:00 +0530

    Update character_arrays.cpp

--- Arrays/character_arrays.cpp
+++ Arrays/character_arrays.cpp
@@ -41,3 +41,40 @@ e
 s
 h
 ####################################################################################################################
+#include<iostream>
+using namespace std;
+void display(char *arr)
+{
+    cout<<*arr<<endl;
+    // the only difference between integer array and character array is if we 
+    //print arr int array prints the address where as the character array prints 
+    // the string in that address till null.
+    cout<<arr<<endl;
+    for(int i=0;i<12;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+int main()
+{
+    char arr[]="vignesh babu";
+    display(arr);
+    
+    
+}
+#################################################################################################################################
+v
+vignesh babu
+v
+i
+g
+n
+e
+s
+h
+ 
+b
+a
+b
+u
+######################################################################################################################################
