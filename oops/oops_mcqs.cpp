b38df1bf43ce1ba97cc3f4d7cf2896a0957ede7e
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-18 17:24:32 +0530

    Update oops_mcqs.cpp

--- oops/oops_mcqs.cpp
+++ oops/oops_mcqs.cpp
@@ -31,3 +31,119 @@ output:
 compiler error
 since the variable is private
 #######################################################################################################################################
+What will be the Output ?
+#include <iostream>
+using namespace std;
+
+class Box{
+    public :
+    int width;
+    int height;
+    int length;
+    void volume(){
+        cout << length * width * height;
+    }
+};
+
+int main() {
+    Box b;
+    b.height = 5;
+    b.width = 4;
+    b.volume();
+}
+####################################################################################################################
+output:
+Garbage value.
+#################################################################################################################################
+#include <iostream>
+using namespace std;
+
+class Student{
+    public :
+    char name;
+    int rollNo;
+
+    Student(int num){
+        rollNo = num;
+    }
+
+    void print(){
+        cout << name << " "  <<  rollNo;
+    }
+};
+
+int main() {
+    Student *s = new Student();
+    s -> name = 'A';
+    s -> rollNo = 15;
+    s -> print();
+}
+###############################################################################
+output:
+Compilation error
+this will throw the compilation error since the matching constructor is not found.
+################################################################################################################################
+Find Incorrect Statement
+Send Feedback
+Which of the following statements are not true about destructor?
+1. It is invoked when object goes out of the scope
+2. Like constructor, it can also have parameters 
+3. It can be declared in private section
+4. It bears same name as that of the class and precedes tilde sign. 
+######################################################################################
+output:
+2
+###############################################################################################################################
+Copy Assignment Operator
+Send Feedback
+Consider following piece of code:
+class Pair{
+  public :
+  int first;
+  int second;
+};
+
+int main(){
+
+  Pair *p1 = new Pair;
+  Pair p2;
+}
+Which of the following statement is feasible ?\
+############################################################
+p2=*p1;
+##################################################################################################################################
+Copy Assignment Operator
+Send Feedback
+Consider following piece of code:
+#include<iostream>
+using namespace std;
+
+class Pair{
+      public :
+      int first;
+      int second;
+};
+
+int main(){
+
+      Pair p1;
+      p1.first = 10;
+      p1.second = 20;
+      Pair p2;
+      p2 = p1;
+      cout << p2.first << " " << p2.second << endl;
+}
+What will be the output ?
+##################################################################
+output:
+10 20
+##################################################################################################################################
+
+
+
+
+
+
+
+
+
