dcce22eda2286a8dbffbda609e55399db596fab9
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-18 23:22:41 +0530

    Create strings_noobs.cpp

---
+++ Strings/strings_noobs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+
+int main()
+{
+    string s="vignesh babu";
+    char arr[]="vignesh babu";
+    cout<<s<<endl;
+    cout<<arr<<endl;
+    cout<<s.length()<<endl;
+    cout<<strlen(arr)<<endl;
+    
+}
+####################################################################################################################
+output:
+vignesh babu
+vignesh babu
+12
+12
+#################################################################################################################
+we use the new keyword to create them dynamcially.
+################################################################################################################
