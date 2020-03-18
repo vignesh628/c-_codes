7d167ced0e29f4bad1a0f269e1bbf79e2132632a
GANDHAM VIGNESH BABU <vignesh943628@gmail.com>
2020-03-18 23:17:19 +0530

    Create arrays_noobs.cpp

---
+++ Arrays/arrays_noobs.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+#include<string.h>
+
+int main()
+{
+    string s="vignesh babu";
+    char arr[]="vignesh babu";
+    cout<<s<<endl;
+    cout<<arr<<endl;
+    cout<<s.length()<<endl;
+    cout<<strlen(arr)<<endl;
+    
+}
+####################################################################################################
+output:
+vignesh babu
+vignesh babu
+12
+12
+#####################################################################################################################################
